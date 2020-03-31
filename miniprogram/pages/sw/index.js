b30023f9a3898a6bac29c4c3b280b52b93ed116d
liliang <zkyeu@163.com>
2020-03-31 16:27:27 +0800

    小程序

---
+++ miniprogram/pages/sw/index.js
@@ -0,0 +1,7 @@
+Page({
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true
+  },
+  
+})
---
+++ miniprogram/utils/utils.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+  console.log(`Hello ${name} !`)
+}
+function sayGoodbye(name) {
+  console.log(`Goodbye ${name} !`)
+}
+
+module.exports = { 
+  sayHello,
+  sayGoodbye
+};
